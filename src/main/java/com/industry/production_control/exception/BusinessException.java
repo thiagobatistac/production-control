fdf14a41dd4652137140a650c90b874c7e951c7c
Thiago Coutinho <batista.thiago2002@gmail.com>
2026-02-03 12:52:28 -0300

    Add business exceptions, global exception handler, and CORS configuration

---
+++ src/main/java/com/industry/production_control/exception/BusinessException.java
@@ -0,0 +1,11 @@
+package com.industry.production_control.exception;
+
+public class BusinessException extends RuntimeException {
+    public BusinessException(String message) {
+        super(message);
+    }
+
+    public BusinessException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/industry/production_control/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.industry.production_control.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resourceName, Long id) {
+        super(String.format("%s not found with id: %d", resourceName, id));
+    }
+}
