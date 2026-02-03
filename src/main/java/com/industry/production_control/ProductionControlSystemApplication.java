654b43cd3e9a32a1d010c1f445cc765eeb431576
Thiago Coutinho <batista.thiago2002@gmail.com>
2026-02-03 11:20:11 -0300

    initial commit - project structure and packages

---
+++ src/main/java/com/industry/production_control/ProductionControlSystemApplication.java
@@ -0,0 +1,13 @@
+package com.industry.production_control;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductionControlSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductionControlSystemApplication.class, args);
+	}
+
+}
